117ab26a2741b5cf712e037fedf0a749826b0c28
ctrly4sh <tiwariyash2207@gmail.com>
2025-08-12 14:51:03 +0530

    Cors and Rate limiting

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,31 @@
 import express, {Application, Response} from "express"
 import authRouter from "./routes/auth.Routes";
 import userRouter from "./routes/user.Routes";
+import helmet from "helmet";
+import cors from "cors";
+import rateLimit from "express-rate-limit";
 
 const app: Application = express()
+
+app.use(helmet({
+    contentSecurityPolicy: false,
+    crossOriginResourcePolicy: false,
+}));
+
+
+app.use(cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true   
+}));
+
+const rateLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, //15 minutes
+    max: 10,
+    message : "Too many requests from this IP, please try again after 15 minutes"
+})
+
+app.use(rateLimiter);
 
 app.use(express.json());
 
